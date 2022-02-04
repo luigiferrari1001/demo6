587bb9a1a5aeb45e790db4beb2f6b50717e39e25
Luigi <lferrari@calvino.ge.it>
2022-02-04 16:50:02 +0100

    Initial commit

---
+++ src/main/java/com/gmail/lestopasso/demo6/HelloController.java
@@ -0,0 +1,26 @@
+package com.gmail.lestopasso.demo6;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private ImageView view;
+
+    @FXML
+    public void initialize(){
+        view.setImage(
+                new Image(".\\Cars-Luigi.jpg")
+        );
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.gmail.lestopasso.demo6 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.gmail.lestopasso.demo6 to javafx.fxml;
+    exports com.gmail.lestopasso.demo6;
+}
